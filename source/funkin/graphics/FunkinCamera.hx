1eed5e701583ac4ccb83482ebc55d4d3bbcb9b47
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-01 06:04:43 -0300

    Clean up comments in FunkinCamera.hx
    Removed extensive comments and documentation regarding blend modes and shader support from the FunkinCamera class.

--- source/funkin/graphics/FunkinCamera.hx
+++ source/funkin/graphics/FunkinCamera.hx
@@ -19,32 +19,9 @@ import flixel.graphics.tile.FlxDrawTrianglesItem;
 
 using funkin.graphics.framebuffer.BitmapDataUtil;
 
-/**
- * A FlxCamera with additional powerful features:
- * - Added the ability to grab the camera screen as a `BitmapData` and use it as a texture.
- * - Added support for the following blend modes for a sprite through shaders:
- *   - DARKEN
- *   - HARDLIGHT
- *   - LIGHTEN
- *   - OVERLAY
- *   - DIFFERENCE
- *   - INVERT
- *   - COLORDODGE
- *   - COLORBURN
- *   - SOFTLIGHT
- *   - EXCLUSION
- *   - HUE
- *   - SATURATION
- *   - COLOR
- *   - LUMINOSITY
- */
 @:nullSafety @:access(openfl.display.DisplayObject) @:access(openfl.display.BitmapData) @:access(openfl.display3D.Context3D) @:access(openfl.display3D.textures.TextureBase) @:access(flixel.graphics.FlxGraphic) @:access(flixel.graphics.frames.FlxFrame) @:access(openfl.display.OpenGLRenderer) @:access(openfl.geom.ColorTransform)
 class FunkinCamera extends FlxCamera
 {
-  /**
-   * Whether or not the device supports the OpenGL extension `KHR_blend_equation_advanced`.
-   * If `false`, a shader implementation will be used to render certain blend modes.
-   */
   public static var hasKhronosExtension(get, never):Bool;
 
   static inline function get_hasKhronosExtension():Bool
@@ -57,13 +34,6 @@ class FunkinCamera extends FlxCamera
     #end
   }
 
-  /**
-   * A list of blend modes that require the OpenGL extension `KHR_blend_equation_advanced`.
-   *
-   * NOTE:
-   *  - `LIGHTEN` is supported natively on desktop, but not other platforms.
-   *  - While `DARKEN` is supported natively on desktop, it causes issues with transparency.
-   */
   static final KHR_BLEND_MODES:Array<BlendMode> = [
     DARKEN,
     HARDLIGHT,
@@ -80,23 +50,10 @@ class FunkinCamera extends FlxCamera
     LUMINOSITY
   ];
 
-  /**
-   * A list of blend modes that require the shader no matter what.
-   * This is due to these blend modes not being supported on any platform.
-   */
   static final SHADER_REQUIRED_BLEND_MODES:Array<BlendMode> = [INVERT];
 
-  /**
-   * The ID of this camera, used for debugging.
-   */
   public var id:String;
 
-  /**
-   * If `true` the blend shader will try to blend with the cameras underneath it.
-   * This is useful for, say, making a strumline note have a shader-only blend mode like `INVERT`.
-   *
-   * Defaults to `false` since this can impact performance.
-   */
   public var crossCameraBlending:Bool;
 
   var _blendShader:RuntimeCustomBlendShader;
@@ -132,20 +89,16 @@ class FunkinCamera extends FlxCamera
   {
     var shouldUseShader:Bool = (!hasKhronosExtension && KHR_BLEND_MODES.contains(blend)) || SHADER_REQUIRED_BLEND_MODES.contains(blend);
 
-    // Fallback to the shader implementation if the device doesn't support `KHR_blend_equation_advanced`, or if
-    // the specified blend mode requires the shader.
     if (shouldUseShader)
     {
       if (crossCameraBlending)
       {
+        var myIndex:Int = FlxG.cameras.list.indexOf(this);
         var camerasUnderneath:Array<FlxCamera> = FlxG.cameras.list.copy();
 
-        for (i in camerasUnderneath.length - 1...-1)
+        if (myIndex >= 0)
         {
-          if (i > FlxG.cameras.list.indexOf(this))
-          {
-            camerasUnderneath.remove(camerasUnderneath[i]);
-          }
+          camerasUnderneath = camerasUnderneath.slice(0, myIndex);
         }
 
         _cameraTexture.drawCameraScreens(camerasUnderneath);
@@ -163,8 +116,6 @@ class FunkinCamera extends FlxCamera
 
       _backgroundFrame.frame.set(0, 0, this.width, this.height);
 
-      // Clear the camera's graphics
-      // It'll get redrawn anyway
       this.clearDrawStack();
       this.canvas.graphics.clear();
 
@@ -190,8 +141,6 @@ class FunkinCamera extends FlxCamera
 
       _backgroundFrame.parent.bitmap = _blendRenderTexture.graphic.bitmap;
 
-      // On some displays, the DPI can be less than 1, which causes the blend shader to look bad
-      // We just clamp the scale to 1 to avoid this!
       var clampedScale:Float = Math.max(1, Lib.current.stage.window.scale);
 
       _backgroundRenderTexture.init(Std.int(this.width * clampedScale), Std.int(this.height * clampedScale));
@@ -204,7 +153,6 @@ class FunkinCamera extends FlxCamera
 
       _backgroundRenderTexture.render();
 
-      // Resize the frame so it always fills the screen
       _cameraMatrix.identity();
       _cameraMatrix.scale(1 / (this.scaleX * clampedScale), 1 / (this.scaleY * clampedScale));
       _cameraMatrix.translate(((width - width / this.scaleX) * 0.5), ((height - height / this.scaleY) * 0.5));
@@ -220,7 +168,6 @@ class FunkinCamera extends FlxCamera
   override function startQuadBatch(graphic:FlxGraphic, colored:Bool, hasColorOffsets:Bool = false, ?blend:BlendMode, smooth:Bool = false,
       ?shader:FlxShader):FlxDrawQuadsItem
   {
-    // Can't batch complex non-coherent blends, so always force a new batch
     if (hasKhronosExtension && !(OpenGLRenderer.__coherentBlendsSupported ?? false) && KHR_BLEND_MODES.contains(blend))
     {
       var itemToReturn = null;
@@ -237,7 +184,6 @@ class FunkinCamera extends FlxCamera
         itemToReturn = new FlxDrawQuadsItem();
       }
 
-      // TODO: catch this error when the dev actually messes up, not in the draw phase
       if (graphic.isDestroyed) throw 'Cannot queue ${graphic.key}. This sprite was destroyed.';
 
       itemToReturn.graphics = graphic;
@@ -272,7 +218,6 @@ class FunkinCamera extends FlxCamera
   override function startTrianglesBatch(graphic:FlxGraphic, smoothing:Bool = false, isColored:Bool = false, ?blend:BlendMode, ?hasColorOffsets:Bool,
       ?shader:FlxShader):FlxDrawTrianglesItem
   {
-    // Can't batch complex non-coherent blends, so always force a new batch
     if (hasKhronosExtension
       && !(OpenGLRenderer.__coherentBlendsSupported ?? false)
       && KHR_BLEND_MODES.contains(blend)) return getNewDrawTrianglesItem(graphic, smoothing, isColored, blend, hasColorOffsets, shader);
@@ -288,5 +233,9 @@ class FunkinCamera extends FlxCamera
     _backgroundRenderTexture.destroy();
 
     _cameraTexture.dispose();
+
+    if (_backgroundFrame?.parent != null) _backgroundFrame.parent.destroy();
+
+    _blendShader = null;
   }
 }
