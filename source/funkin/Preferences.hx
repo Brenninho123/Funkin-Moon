33394160c2704b38ace7547fb222e3755010873b
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-06 09:54:55 -0300

    Implement mode3D option in Preferences
    Add getter and setter for mode3D option in Preferences.

--- source/funkin/Preferences.hx
+++ source/funkin/Preferences.hx
@@ -155,6 +155,21 @@ class Preferences
     return value;
   }
 
+  public static var mode3D(get, set):Bool;
+
+  static function get_mode3D():Bool
+  {
+    return Save?.instance?.options?.mode3D ?? false;
+  }
+
+  static function set_mode3D(value:Bool):Bool
+  {
+    var save:Save = Save.instance;
+    save.options.mode3D = value;
+    Save.system.flush();
+    return value;
+  }
+
   public static var storageType(get, set):String;
 
   static function get_storageType():String
