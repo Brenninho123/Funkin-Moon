3b66970b2193f5405e8df6fa091831693d3a1180
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-12 09:52:24 -0300

    Update backButton initialization with confirmCallback

--- source/funkin/ui/options/OptionsState.hx
+++ source/funkin/ui/options/OptionsState.hx
@@ -300,7 +300,7 @@ class OptionsMenu extends Page<OptionsMenuPageName>
     #if NO_FEATURE_TOUCH_CONTROLS
     createItem('EXIT', exit);
     #else
-    backButton = new FunkinBackButton(FlxG.width - 230, FlxG.height - 200, exit, 1.0);
+    backButton = new FunkinBackButton(FlxG.width - 230, FlxG.height - 200, confirmCallback: exit);
     backButton.onConfirmStart.add(function()
     {
       items.busy = true;
