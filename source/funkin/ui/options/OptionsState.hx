25ffe2d6f6c44a4d58fc08a391b0ffbec17f5efd
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-12 05:59:15 -0300

    Add version text to OptionsState and remove comments
    Added version text display to OptionsState and cleaned up comments.

--- source/funkin/ui/options/OptionsState.hx
+++ source/funkin/ui/options/OptionsState.hx
@@ -5,8 +5,7 @@ import funkin.ui.transition.LoadingState;
 import funkin.ui.TextMenuList;
 import funkin.ui.TextMenuList.TextMenuItem;
 import flixel.math.FlxPoint;
-import funkin.ui.TextMenuList;
-import funkin.ui.TextMenuList.TextMenuItem;
+import flixel.text.FlxText;
 import flixel.FlxSprite;
 import flixel.FlxObject;
 import flixel.FlxState;
@@ -32,16 +31,8 @@ import funkin.mobile.util.InAppPurchasesUtil;
 #end
 import flixel.util.FlxColor;
 
-/**
- * The main options menu
- * It mainly is controlled via the "optionsCodex" object,
- * which handles paging and going to the different submenus
- */
 class OptionsState extends MusicBeatState
 {
-  /**
-   * Instance of the OptionsState
-   */
   public static var instance:OptionsState;
 
   var optionsCodex:Codex<OptionsMenuPageName>;
@@ -50,6 +41,8 @@ class OptionsState extends MusicBeatState
 
   public static var rememberedSelectedIndex:Int = 0;
   public static var backState:Null<String> = null;
+
+  var versionText:FlxText;
 
   override function create():Void
   {
@@ -94,7 +87,6 @@ class OptionsState extends MusicBeatState
     }
     else
     {
-      // No need to show Options page
       #if FEATURE_TOUCH_CONTROLS
       preferences.onExit.add(exitToMainMenu);
       optionsCodex.setPage(Preferences);
@@ -103,6 +95,15 @@ class OptionsState extends MusicBeatState
       optionsCodex.setPage(Controls);
       #end
     }
+
+    versionText = new FlxText(0, 0, 300, 'Mobile Port v0.8.5 - By Brenninho', 16);
+    versionText.setFormat(null, 16, FlxColor.WHITE, RIGHT);
+    versionText.scrollFactor.set(0, 0);
+    versionText.x = FlxG.width - versionText.width - 12;
+    versionText.y = FlxG.height - versionText.height - 8;
+    versionText.zIndex = 10000;
+    versionText.alpha = 0.75;
+    add(versionText);
 
     super.create();
     #if FEATURE_TOUCH_CONTROLS
@@ -132,7 +133,6 @@ class OptionsState extends MusicBeatState
 
   function exitControls():Void
   {
-    // Apply any changes to the controls.
     PlayerSettings.reset();
     PlayerSettings.init();
 
@@ -142,7 +142,6 @@ class OptionsState extends MusicBeatState
   function exitToMainMenu():Void
   {
     optionsCodex.currentPage.enabled = false;
-    // TODO: Animate this transition?
     if (backState != null)
     {
       var state:MusicBeatState = MusicBeatState.scriptInit(backState);
@@ -161,9 +160,6 @@ class OptionsState extends MusicBeatState
   }
 }
 
-/**
- * Our default Page when we enter the OptionsState, a bit of the root
- */
 class OptionsMenu extends Page<OptionsMenuPageName>
 {
   var items:TextMenuList;
@@ -172,9 +168,6 @@ class OptionsMenu extends Page<OptionsMenuPageName>
   var goingBack:Bool = false;
   #end
 
-  /**
-   * Camera focus point
-   */
   var camFocusPoint:FlxObject;
 
   final CAMERA_MARGIN:Int = 150;
@@ -189,9 +182,6 @@ class OptionsMenu extends Page<OptionsMenuPageName>
     if (ControlsHandler.hasExternalInputDevice)
     #end
     createItem('CONTROLS', function() codex.switchPage(Controls));
-    // createItem("CONTROL SCHEMES", function() {
-    //   FlxG.state.openSubState(new ControlsSchemeMenu());
-    // });
     #if FEATURE_LAG_ADJUSTMENT
     createItem('LAG ADJUSTMENT', function()
     {
@@ -240,8 +230,6 @@ class OptionsMenu extends Page<OptionsMenuPageName>
       {
         NewgroundsClient.instance.logout(function()
         {
-          // Reset the options menu when logout succeeds.
-          // This means the login option will be displayed.
           FlxG.resetState();
         }, function()
         {
@@ -255,10 +243,6 @@ class OptionsMenu extends Page<OptionsMenuPageName>
       {
         NewgroundsClient.instance.login(function()
         {
-          // Reset the options menu when login succeeds.
-          // This means the logout option will be displayed.
-          // NOTE: If the user presses login and opens the browser,
-          // then navigates the UI
           FlxG.resetState();
         }, function()
         {
@@ -268,16 +252,13 @@ class OptionsMenu extends Page<OptionsMenuPageName>
     }
     #end
 
-    // Create an object for the camera to track.
     camFocusPoint = new FlxObject(0, 0, 140, 70);
     add(camFocusPoint);
 
-    // Follow the camera focus as we scroll.
     FlxG.camera.follow(camFocusPoint, null, 0.085);
     FlxG.camera.deadzone.set(0, CAMERA_MARGIN / 2, FlxG.camera.width, FlxG.camera.height - CAMERA_MARGIN + 40);
     FlxG.camera.minScrollY = -CAMERA_MARGIN / 2;
 
-    // Move the camera when the menu is scrolled.
     items.onChange.add(onMenuChange);
 
     onMenuChange(items.members[0]);
@@ -290,7 +271,6 @@ class OptionsMenu extends Page<OptionsMenuPageName>
 
   public function addSaveDataOptionsItem(saveDataMenu:SaveDataMenu):Void
   {
-    // no need to show an entire new menu for just one option
     if (saveDataMenu.hasMultipleOptions())
     {
       createItem('SAVE DATA OPTIONS', function()
@@ -352,10 +332,6 @@ class OptionsMenu extends Page<OptionsMenuPageName>
     return super.set_enabled(value);
   }
 
-  /**
-   * True if this page has multiple options, excluding the exit option.
-   * If false, there's no reason to ever show this page.
-   */
   public function hasMultipleOptions():Bool
   {
     return items.length > 2;
@@ -376,7 +352,6 @@ class OptionsMenu extends Page<OptionsMenuPageName>
     add(prompt);
     prompt.onYes = function()
     {
-      // Clear the save data.
       funkin.save.Save.clearData();
       FlxG.switchState(() -> new funkin.InitState());
     };
